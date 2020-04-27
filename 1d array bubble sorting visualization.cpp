b7a6d17dad349e6e8047c1e428ac463cd7566218
LaHesis <cryptograph2013@yandex.ru>
2020-04-27 17:46:54 +0700

    saving animation
    1. added saveAnimation function that converts frames into a gif;
    2. added frame name encoding to ensure correct frame order;
    3. now sorting function returns comparison/swap amount;
    4. added 2 additional frames for unsorted and sorted arrays apiece;
    5. added removing dir with frames;
    6. defining pixel RGB-components separated into function;
    7. new named constants exposed;
    8. refactoring.

--- 1d array bubble sorting visualization.cpp
+++ 1d array bubble sorting visualization.cpp
@@ -1,15 +1,27 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <windows.h>
 #include <time.h>
 #include <png.h>
 
-#define N 40
-// In pixels. Min is 2.
-#define IMAGE_WIDTH 1366
+// Array element and color block amount.
+#define N 35
+// In pixels.
+#define IMAGE_WIDTH 900
+// Whether to show blocks with the same value
+// but colored with red, green and blue.
 #define WITH_STABILITY_SHOWN 1
-#define SORTING_IMG_DIR "sorting img"
+// Where to save frames.
+#define FRAMES_DIR "animation frames"
+// Where to save animation files.
+#define ANIMATIONS_DIR "animations"
+// Delay between frames, in 1/100 second.
+#define ANIMATION_DELAY "3"
+// Transitions between frames.
+#define ANIMATION_MORPH "3"
 
+#define BACKGROUND_LIGHTNESS 250
 #define MIN_LIGHTNESS 90
 #define MAX_LIGHTNESS 145
 #define STAB_LIGHTNESS 230
@@ -17,6 +29,7 @@
 #define BORDER_ADDITION_MIN_WIDTH 7
 #define BORDER_LIGHTNESS 20
 const short BLOCK_WIDTH = IMAGE_WIDTH / N;
+char quotedFramesDir[25];
 
 enum stabilityColors {
     regularColor,
@@ -31,20 +44,19 @@ typedef struct {
     short startPos;
 } colorBlock;
 
-void printArrayIntoImage(colorBlock*, short, short, short, char*);
-void paintPixelWithColorRatio(unsigned char*, float);
+void printArrayIntoPNGFrame(colorBlock* blocks, int, short, short);
+void saveAnimation(int);
 void printArrayIntoConsole(colorBlock*);
-void paintPixelWithColorRatio(unsigned char* pixelRGB, float colorRatio);
+void paintPixel(unsigned char*, unsigned char, unsigned char, unsigned char);
 void shuffleArray(colorBlock*);
 colorBlock* generateArray();
-void bubbleSort(colorBlock*);
+int bubbleSortAndGetStepCount(colorBlock*);
+unsigned char* encodeNumberIntoString(int);
 
 
-int main()
-{
+int main() {
     system("chcp 1251");
     colorBlock* colorBlocks = generateArray();
-
     printf("initial:\n");
     printArrayIntoConsole(colorBlocks);
 
@@ -52,40 +64,74 @@ int main()
     printf("after shuffling:\n");
     printArrayIntoConsole(colorBlocks);
     
-    char createDirCommand[30];
-    sprintf_s(createDirCommand, 30, "mkdir \"%s\"", SORTING_IMG_DIR);
+    sprintf_s(quotedFramesDir, 27, "\"%s\"", FRAMES_DIR);
+    char removeDirCommand[35];
+    sprintf_s(removeDirCommand, 35, "rmdir %s /S /Q", quotedFramesDir);
+    system(removeDirCommand);
+
+    char createDirCommand[35];
+    sprintf_s(createDirCommand, 35, "mkdir %s", quotedFramesDir);
     system(createDirCommand);
-    bubbleSort(colorBlocks);
+
+    int stepsCount = bubbleSortAndGetStepCount(colorBlocks);
     printf("after sortings:\n");
     printArrayIntoConsole(colorBlocks);
+
+    printf("creating animation from frames");
+    saveAnimation(stepsCount);
     return 0;
 }
 
-void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedBlockInd, short smallerSwappedBlockInd, char *fileDir) {
+unsigned char* encodeNumberIntoString(int num) {
+    const short usedCharsCount = 36;
+    short length = num / usedCharsCount + 1;
+    unsigned char* string = (unsigned char*)malloc(sizeof(unsigned char) * (length + 1));
+    int i = 0;
+    while (num > usedCharsCount - 1) {
+        string[i++] = 'Z';
+        num -= usedCharsCount;
+    }
+    short remainder = num % usedCharsCount;
+    if (remainder < 10) {
+        // '0' - '9' mapping.
+        string[i++] = (unsigned char) (48 + (remainder));
+    } else if (remainder < 36) {
+        // 'A' - 'Z' mapping.
+        string[i++] = (unsigned char) (65 + (remainder - 10));
+    }
+    string[i] = '\0';
+    return string;
+}
+
+void printArrayIntoPNGFrame(colorBlock *blocks, int frmNum, short greaterSwappedBlockInd, short smallerSwappedBlockInd) {
+    unsigned char fileName[100];
+    // If to use numbers for frame names there is problem with wrong frames order.
+    unsigned char* encodedNum = encodeNumberIntoString(frmNum);
+    sprintf_s(fileName, 100, "%s\\%s.png", FRAMES_DIR, encodedNum);
+    
     FILE* fp;
-    char fileName[100];
-    sprintf_s(fileName, 100, "%s\\image%d.png", fileDir, imgNum);
+    printf("trying to save step %d as \"%s\"\n", frmNum, fileName);
     fopen_s(&fp, fileName, "wb");
+    free(encodedNum);
 
     // Errors checking.
     if (!fp) {
         printf("failed to write into file");
-        return;
+        exit(1);
     }
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     if (!png_ptr) {
         printf("failed to create write struct");
         fclose(fp);
-        return;
+        exit(1);
     }
     png_infop png_info;
     if (!(png_info = png_create_info_struct(png_ptr)) || setjmp(png_jmpbuf(png_ptr))) {
         printf("failed to create info struct");
         png_destroy_write_struct(&png_ptr, NULL);
         fclose(fp);
-        return;
+        exit(1);
     }
-
     
     // In pixels.
     short imgWidthComputed = N*BLOCK_WIDTH;
@@ -96,13 +142,14 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
         PNG_FILTER_TYPE_DEFAULT);
 
     unsigned char** rows = (unsigned char**) malloc(sizeof(unsigned char*) * imgHeight);
+    // Background drawing.
     for (int i = 0; i < imgHeight; i++) {
         unsigned char *row = malloc(sizeof(unsigned char) * imgWidthComputed * 3);
         rows[i] = row;
         for (short j = 0; j < imgWidthComputed * 3; j++) {
             float color = j;
             color = 250;
-            row[j] = (unsigned char) color;
+            row[j] = (unsigned char)color;
         }
     }
 
@@ -117,10 +164,11 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
     unsigned char bottomBorderIsHere = 0;
     unsigned char borderUnderCapIsHere = 0;
 
+    // Drawing each color block.
     for (short blockInd = 0; blockInd < N; blockInd++) {
         colorBlock clrBl = *(blocks + blockInd);
         short rowAboveBlockTop = imgHeight - (clrBl.value + 1) * rowsInOneVal;
-        short rowBelowBlockCap = rowAboveBlockTop + rowsInOneVal * .7 + 1;
+        short rowBelowBlockCap = (short)(rowAboveBlockTop + rowsInOneVal * .7 + 1);
         unsigned char isBlockToEmphasize = (greaterSwappedBlockInd == blockInd || smallerSwappedBlockInd == blockInd);
         for (short i = imgHeight - 1; i > rowAboveBlockTop; i--) {
             short blockStartPos = blockInd * BLOCK_WIDTH * 3;
@@ -130,6 +178,7 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
             float verticalColorRatio = isBlockToEmphasize ? (imgHeight - i) / (float)(imgHeight - rowBelowBlockCap) : 0;
             // Color blocks are painted with gradient according to clrBl.value as a whole.
             float horizontalColorRatio = clrBl.value / ((float)N - 1);
+            unsigned char clrVal = (unsigned char)MAX_LIGHTNESS * horizontalColorRatio + MIN_LIGHTNESS;
 
             // Horizontal borders.
             if (whetherShowBorders) {
@@ -146,35 +195,26 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
                     rightBorderIsHere = (j == (blockInd + 1) * BLOCK_WIDTH * 3 - 3 && whetherShowBorders);
                 }
                 if (leftBorderIsHere || rightBorderIsHere || topBorderIsHere || bottomBorderIsHere || borderUnderCapIsHere) {
-                    *pixelRGB = BORDER_LIGHTNESS;
-                    *(pixelRGB + 1) = BORDER_LIGHTNESS;
-                    *(pixelRGB + 2) = BORDER_LIGHTNESS;
+                    paintPixel(pixelRGB, BORDER_LIGHTNESS, BORDER_LIGHTNESS, BORDER_LIGHTNESS);
                 } else {
                     if (WITH_STABILITY_SHOWN) {
                     switch (clrBl.stabColor) {
                         case red:
-                            *pixelRGB = STAB_LIGHTNESS;
-                            *(pixelRGB + 1) = 0;
-                            *(pixelRGB + 2) = 0;
+                            paintPixel(pixelRGB, STAB_LIGHTNESS, 0, 0);
                             break;
                         case green:
-                            *pixelRGB = 0;
-                            *(pixelRGB + 1) = STAB_LIGHTNESS;
-                            *(pixelRGB + 2) = 0;
+                            paintPixel(pixelRGB, 0, STAB_LIGHTNESS, 0);
                             break;
                         case blue:
-                            *pixelRGB = 0;
-                            *(pixelRGB + 1) = 0;
-                            *(pixelRGB + 2) = STAB_LIGHTNESS;
+                            paintPixel(pixelRGB, 0, 0, STAB_LIGHTNESS);
                             break;
                         default:
-                            paintPixelWithColorRatio(pixelRGB, horizontalColorRatio);
+                            paintPixel(pixelRGB, clrVal, clrVal, clrVal);
                             break;
                         }
                     }
-                    else {
-                        paintPixelWithColorRatio(pixelRGB, horizontalColorRatio);
-                    }
+                    else
+                        paintPixel(pixelRGB, clrVal, clrVal, clrVal);
 
                     // Emphasize swapping or comparing blocks.
                     if (isBlockToEmphasize) {
@@ -186,13 +226,9 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
                     // Emphasize block cap.
                     if (i < rowBelowBlockCap) {
                         if (isBlockToEmphasize) {
-                            *pixelRGB = 255;
-                            *(pixelRGB + 1) = 255;
-                            *(pixelRGB + 2) = 170;
+                            paintPixel(pixelRGB, 255, 255, 170);
                         } else {
-                            *pixelRGB = 137;
-                            *(pixelRGB + 1) = 169;
-                            *(pixelRGB + 2) = 169;
+                            paintPixel(pixelRGB, 137, 169, 169);
                         }
                     }
                 }
@@ -209,14 +245,39 @@ void printArrayIntoImage(colorBlock *blocks, short imgNum, short greaterSwappedB
     }
     free(rows);
     fclose(fp);
-    printf("\"%s\" saved\n", fileName);
     png_destroy_write_struct(&png_ptr, NULL);
 }
 
-void paintPixelWithColorRatio(unsigned char* pixelRGB, float colorRatio) {
-    *pixelRGB = MAX_LIGHTNESS * colorRatio + MIN_LIGHTNESS;
-    *(pixelRGB + 1) = MAX_LIGHTNESS * colorRatio + MIN_LIGHTNESS;
-    *(pixelRGB + 2) = MAX_LIGHTNESS * colorRatio + MIN_LIGHTNESS;
+void paintPixel(unsigned char* pixelRGB, unsigned char r, unsigned char g, unsigned char b) {
+    *pixelRGB = r;
+    *(pixelRGB + 1) = g;
+    *(pixelRGB + 2) = b;
+}
+
+void saveAnimation(int stepsCount) {
+    char createDirCommand[18];
+    sprintf_s(createDirCommand, 18, "mkdir %s", ANIMATIONS_DIR);
+    system(createDirCommand);
+
+    // Parameters info.
+    char imageWidth[11] = { 0 };
+    sprintf_s(imageWidth, 11, "width_%d", IMAGE_WIDTH);
+    char elementsAmount[18] = { 0 };
+    sprintf_s(elementsAmount, 18, "%d_elements", N);
+    char stability[16] = { 0 };
+    if (WITH_STABILITY_SHOWN)
+        sprintf_s(stability, 16, "stability_shown");
+    char stepsCountString[25] = { 0 };
+    sprintf_s(stepsCountString, 25, "%d_steps", stepsCount);
+
+    char name[120] = { 0 };
+    sprintf_s(name, 120, "%s %s %s %s", imageWidth, elementsAmount, stability, stepsCountString);
+
+    char *createAnimationCommand[201];
+    // GraphicsMagick utility "convert". I.e. GraphicsMagick should be installed.
+    sprintf_s(createAnimationCommand, 201, "gm convert -morph %s -delay %s \"%s\\*.png\" \"%s\\%s.gif\"", ANIMATION_MORPH, ANIMATION_DELAY, FRAMES_DIR, ANIMATIONS_DIR, name);
+    printf("\ncommand for creating animation: %s\n", createAnimationCommand);
+    system(createAnimationCommand);
 }
 
 void printArrayIntoConsole(colorBlock* ar) {
@@ -288,21 +349,26 @@ void shuffleArray(colorBlock* blocks) {
     }
 }
 
-void bubbleSort(colorBlock* blocks) {
-    short iterNum = 0;
-    printArrayIntoImage(blocks, iterNum++, -1, -1, SORTING_IMG_DIR);
+int bubbleSortAndGetStepCount(colorBlock* blocks) {
+    int stepNum = 0;
+    printArrayIntoPNGFrame(blocks, stepNum++, -1, -1);
+    printArrayIntoPNGFrame(blocks, stepNum++, -1, -1);
+    printArrayIntoPNGFrame(blocks, stepNum++, -1, -1);
     for (short j = 0; j < N; j++) {
         for (short n = 0; n < N - j - 1; n++) {
-            printArrayIntoImage(blocks, iterNum++, n + 1, n, SORTING_IMG_DIR);
+            printArrayIntoPNGFrame(blocks, stepNum++, n + 1, n);
             colorBlock current = *(blocks + n);
             colorBlock next = *(blocks + n + 1);
             if (current.value > next.value) {
                 colorBlock tmp = current;
                 *(blocks + n) = next;
                 *(blocks + n + 1) = tmp;
-                printArrayIntoImage(blocks, iterNum++, n + 1, n, SORTING_IMG_DIR);
+                printArrayIntoPNGFrame(blocks, stepNum++, n + 1, n);
             }
         }
     }
-    printArrayIntoImage(blocks, iterNum, -1, -1, SORTING_IMG_DIR);
+    printArrayIntoPNGFrame(blocks, stepNum++, -1, -1);
+    printArrayIntoPNGFrame(blocks, stepNum++, -1, -1);
+    printArrayIntoPNGFrame(blocks, stepNum, -1, -1);
+    return stepNum - 5;
 }
